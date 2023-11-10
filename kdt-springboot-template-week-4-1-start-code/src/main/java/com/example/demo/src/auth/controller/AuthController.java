fe8f40c49d2154c6332331500610e6a0c7af7399
jola7373 <jola7373@naver.com>
2023-11-10 13:57:37 +0900

    [Feat] auth controller

--- kdt-springboot-template-week-4-1-start-code/src/main/java/com/example/demo/src/auth/controller/AuthController.java
+++ kdt-springboot-template-week-4-1-start-code/src/main/java/com/example/demo/src/auth/controller/AuthController.java
@@ -1,2 +1,18 @@
-package com.example.demo.src.auth.controller;public class AuthController {
+package com.example.demo.src.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @GetMapping("/")
+    public String home() {
+        return "hello, home";
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Hello, Secured";
+    }
+
 }
---
+++ kdt-springboot-template-week-4-1-start-code/src/main/java/com/example/demo/src/config/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.demo.src.config;public class SecurityConfig {
+}
