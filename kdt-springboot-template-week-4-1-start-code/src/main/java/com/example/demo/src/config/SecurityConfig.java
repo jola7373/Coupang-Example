2386e811dee72bcf429be51b5347612e25f9a767
jola7373 <jola7373@naver.com>
2023-11-15 14:40:53 +0900

    [Chore] enableGlobalMethodSecurity추가

--- kdt-springboot-template-week-4-1-start-code/src/main/java/com/example/demo/src/config/SecurityConfig.java
+++ kdt-springboot-template-week-4-1-start-code/src/main/java/com/example/demo/src/config/SecurityConfig.java
@@ -1,28 +1,34 @@
 package com.example.demo.src.config;
 
+import com.example.demo.src.auth.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService oAuth2UserService;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
                 .authorizeHttpRequests(auth->{
                     auth.antMatchers("/").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .oauth2Login(withDefaults())
-                .formLogin(withDefaults())
-                .build();
+                .oauth2Login()
+                .userInfoEndpoint().userService(oAuth2UserService);
+
+        return http.build();
 
     }
 
